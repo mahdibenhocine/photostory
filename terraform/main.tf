90eede23062527085ed3d23b08379ff8cf39cf5a
Mahdi Ben <hocine.mohamed213@gmail.com>
2025-09-06 02:54:28 -0400

    Module02: Added Terraform project with S3 backend, S3 bucket, and CloudFront distribution

---
+++ terraform/main.tf
@@ -0,0 +1,44 @@
+provider "aws" {
+  region = "eu-west-1" # adjust to your chosen region
+}
+
+# Example S3 bucket for website content
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "amir-photostory-s3-repo"   # must be unique globally
+}
+
+# CloudFront distribution with S3 as origin
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
